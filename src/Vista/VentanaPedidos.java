fdcbad58156300cb6ee7edaea66c4b9fa0727c83
rbergoglio <ramiro.bergoglio@gmail.com>
2019-07-03 18:05:48 -0300

    algunos componentes nuevos en ventanaPedidos

--- src/Vista/VentanaPedidos.java
+++ src/Vista/VentanaPedidos.java
@@ -1,5 +1,7 @@
 package Vista;
 
+import java.time.LocalDate;
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 
@@ -14,11 +16,11 @@ public class VentanaPedidos extends JFrame{
 	}
 
 	public JComboBox<String> getBoxProveedor() {
-		return box;
+		return comboBoxProveedores;
 	}
 	
 	public void setBoxProveedor(JComboBox<String> comboBox) {
-		this.box  = comboBox;
+		this.comboBoxProveedores  = comboBox;
 	}
 	
 	public JTextField getTxtProveedor() {
@@ -34,12 +36,13 @@ public class VentanaPedidos extends JFrame{
 	//BOTONES
 	private JTable table;
 	private JTextField txtNroPedido;
-	private JTextField txtEstado;
 	private JTextField txtFecha;
 	private JTextField txtProveedor;
 	JButton btnRegistrar;
 	private JScrollPane scrollPane;
-    private JComboBox<String> box;
+    private JComboBox<String> comboBoxProveedores;
+    private JComboBox<String> comboBoxEstado;
+
     private String[] proveedoresPrueba;
 
 	//
@@ -68,18 +71,39 @@ public class VentanaPedidos extends JFrame{
 			getContentPane().add(lblProveedor);
 			
 			proveedoresPrueba = new String[]{"Proveedor 1", "Proveedor 2", "Proveedor 3","Proveedor 4", "Proveedor 5"};
-			box = new JComboBox<>(proveedoresPrueba);
-			box.setBounds(149, 8, 194, 20);
-			getContentPane().add(box);
+			comboBoxProveedores = new JComboBox<>(proveedoresPrueba);
+			comboBoxProveedores.setBounds(149, 8, 194, 20);
+			getContentPane().add(comboBoxProveedores);
 /*
 			txtProveedor = new JTextField();
 			txtProveedor.setColumns(10);
 			txtProveedor.setBounds(149, 98, 194, 20);
 			getContentPane().add(txtProveedor);
 			*/
+			String[] estados = new String[]{"En tramite","Listo"};
+			comboBoxEstado = new JComboBox<>(estados);
+			comboBoxEstado.setBounds(149, 39, 194, 20);
+			getContentPane().add(comboBoxEstado);
+			
+			
+			JLabel lblEstado = new JLabel("Estado:");
+			lblEstado.setBounds(10, 39, 129, 14);
+			getContentPane().add(lblEstado);
+			
+			txtNroPedido = new JTextField();
+			txtNroPedido.setColumns(10);
+			txtNroPedido.setBounds(149, 67, 194, 20);
+			getContentPane().add(txtNroPedido);
+			
+			JLabel lblNroPedido = new JLabel("NroPedido:");
+			lblNroPedido.setBounds(10, 70, 129, 14);
+			getContentPane().add(lblNroPedido);
+			
+			
 			btnRegistrar = new JButton("Registrar");
 			btnRegistrar.setBounds(353, 7, 89, 23);
 			getContentPane().add(btnRegistrar);
+			
 		
 	}
 }
